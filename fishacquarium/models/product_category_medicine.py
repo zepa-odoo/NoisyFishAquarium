a32a6b004813ca2e3a3fbe82bed7e73cf56cdd15
Zeel Patel <zepa@odoo.com>
2023-02-16 18:55:14 +0530

    model and views modification done

--- fishacquarium/models/product_category_medicine.py
+++ fishacquarium/models/product_category_medicine.py
@@ -5,3 +5,9 @@ class ProductCategoryMedicine(models.Model):
     _description = "medicine category"
 
     name = fields.Char(required=True)
+    vendor_name = fields.Many2one('product.vendor',string="Vendor Name", required=True)
+    product_description = fields.Text(string="Product Description")
+    product_quantity = fields.Integer(string="Avaliable Product")
+    product_price = fields.Float(string="Price")
+    product_img = fields.Image(required=True, string="Product Image", max_width=70, max_height=70)
+    product_color_id = fields.Many2one('product.color',string="Color")
--- fishacquarium/models/product_vendor.py
+++ fishacquarium/models/product_vendor.py
@@ -1,4 +1,4 @@
-from odoo import fields,models
+from odoo import fields,models, api
 from datetime import datetime
 
 class ProductVendor(models.Model):
@@ -11,7 +11,8 @@ class ProductVendor(models.Model):
     company_email = fields.Char(string="Email")
     brand_name = fields.Char(string="Brand Name")
     description = fields.Text(string="Foundation Description")
-    experiance = fields.Integer(string="Experiance", readonly=True)
+    brand_img = fields.Image(string="Brand Image", max_width=70, max_height=70)
+    experiance = fields.Integer(string="Experiance",compute="_compute_experiance")
     product_production_type = fields.Selection(
         string = "Production Type",
         selection = [('natural_product','Natural Product'),('artificial_product','Artificial Product')],
@@ -26,3 +27,12 @@ class ProductVendor(models.Model):
         for y in range(datetime.now().year-30, datetime.now().year):
             year_list.append((str(y), str(y)))
         return year_list
+    
+    @api.depends('company_year')
+    def _compute_experiance(self):
+        for record in self:
+            if record.create_date:
+                record.experiance = fields.date.today().year-int(record.company_year)
+            else:
+                record.experiance=0
+                
